b6ec9bd411c2f2aa381b08ad0c57093b1f7abc86
miriam590 <miriam590@gmail.com>
2025-04-09 15:23:06 +0300

    commit

--- server/testing/conftest.py
+++ server/testing/conftest.py
@@ -1,9 +1,8 @@
-#!/usr/bin/env python3
+import warnings
 
-def pytest_itemcollected(item):
-    par = item.parent.obj
-    node = item.obj
-    pref = par.__doc__.strip() if par.__doc__ else par.__class__.__name__
-    suf = node.__doc__.strip() if node.__doc__ else node.__name__
-    if pref or suf:
-        item._nodeid = ' '.join((pref, suf))
+# Suppress deprecation warnings
+warnings.filterwarnings(
+    "ignore",
+    message="The '__version__' attribute is deprecated",
+    category=DeprecationWarning
+)
